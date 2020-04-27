21148783351fce1b2b09336a6c4812272561a40e
Renlord <me@renlord.com>
2020-04-27 22:04:06 +1000

    ask to install plug.vim as part of bootstrap process

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
+
+source util/ask.sh
 
 set -x
 
@@ -10,5 +12,15 @@ if [ ! -x "$(command -v stow)" ]; then
 fi
 
 stow -v --restow --target="$HOME" --dir="$DOTDIR"/HOME-STOW $(ls $DOTDIR/HOME-STOW)
+
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ] || [ ! -f ~/.nvim/autoload/plug.vim ]; then
+    echo "plug.vim not found..."
+    if ask "Install plug.vim?" Y; then
+        command -v curl && curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        echo "installing plug.vim"
+    else
+        echo "NOT installing plug.vim"
+    fi
+fi
 
 exit 0
